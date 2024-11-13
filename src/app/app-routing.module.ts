ae31724c42f99a65c1b14f94d93aee0609fc5300
Kajaldudhe <kajal.dudhe@shauryatechnosoft.com>
2024-11-13 12:29:05 +0530

    push changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { FormComponent } from "./form/form.component";
 import { BankNameComponent } from "./bank-name/bank-name.component";
+import { UserComponent } from "./user/user.component";
 
 const routes:Routes =[
-    {path:'',redirectTo:'form',pathMatch:'full'},
+    {path:'',redirectTo:'user',pathMatch:'full'},
     {path:'form',component:FormComponent},
-    {path:'bank-name',component:BankNameComponent}
+    {path:'bank-name',component:BankNameComponent},
+    {path:'user', component:UserComponent}
 ]
 
 @NgModule({
